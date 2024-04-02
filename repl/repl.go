5c2992fb3bb35624d22af20fe2f912d3884481af
Michael Ozdoba <mjozdoba@gmail.com>
2024-04-02 13:55:45 -0700

    onto bindings and environement - chapter 3.9 - page 172

--- repl/repl.go
+++ repl/repl.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"writing-an-interpreter-in-go/evaluator"
 	"writing-an-interpreter-in-go/lexer"
-	"writing-an-interpreter-in-go/token"
+	"writing-an-interpreter-in-go/parser"
 )
 
 const PROMPT = ">> "
@@ -22,9 +23,40 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors :\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
